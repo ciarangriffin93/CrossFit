66e2fcc590e02b07d404057917ff5b27f0416700
Ciaran Griffin <griffinciaran3@gmail.com>
2024-06-19 19:55:08 +0000

    Add navbar and images for logo

--- crossfit/settings.py
+++ crossfit/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ciarangriffin9-crossfit-yrfy6bzaegz.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
