151230ea682d1fc2601a6f5d430d492a36dcc277
Ciaran Griffin <griffinciaran3@gmail.com>
2024-06-20 10:48:57 +0000

    Add footer and create css

--- crossfit/settings.py
+++ crossfit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriffin9-crossfit-yrfy6bzaegz.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriffin9-crossfit-3xycm6uvqf3.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
