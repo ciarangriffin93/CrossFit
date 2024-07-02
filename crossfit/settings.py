b0bc545e25733ef1f1c381506f680f5522c36093
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-02 19:39:49 +0000

    Add background color on navbar

--- crossfit/settings.py
+++ crossfit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriffin9-crossfit-4ypnl8g47rp.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriffin9-crossfit-yta6j26t5s6.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
